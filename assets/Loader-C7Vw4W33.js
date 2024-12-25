9b3074aef3e95ef23fba28f28dec006a8247ed2f
Andrii Zakharenko <andriizakharenko@gmail.com>
2024-12-25 23:41:29 +0200

    Updates

---
+++ assets/Loader-C7Vw4W33.js
@@ -0,0 +1,1 @@
+import{j as e,H as l}from"./react-vendor-CfRHzYSL.js";const t=()=>e.jsx(l,{children:e.jsx("div",{className:"absolute top-0 left-0 w-full h-full flex justify-center items-center",children:e.jsx("div",{className:"w-[10vw] h-[10vw] rounded-full",children:"Loading..."})})});export{t as default};
--- assets/Loader-DdVfgNA7.js
+++
@@ -1,1 +0,0 @@
-import{j as e,H as l}from"./react-vendor-D3KG6Cvo.js";const t=()=>e.jsx(l,{children:e.jsx("div",{className:"absolute top-0 left-0 w-full h-full flex justify-center items-center",children:e.jsx("div",{className:"w-[10vw] h-[10vw] rounded-full",children:"Loading..."})})});export{t as default};
